b76ed4b609cba8a1eb45599f95be11264cc00531
kjminn <kjminn2001@gmail.com>
2024-11-12 15:54:13 +0900

    10.1 스프링 시큐리티 이용하는 프로젝트 생성

---
+++ src/main/java/kr/ac/kopo/logintest/config/SecurityConfig.java
@@ -0,0 +1,10 @@
+package kr.ac.kopo.logintest.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Log4j2
+public class SecurityConfig {
+
+}
